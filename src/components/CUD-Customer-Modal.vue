e67e5af95420a701abc0f1f7eb14365f0bb59892
Ebenezer Jesuraj <ebenezer@beezlabs.com>
2022-11-14 12:47:41 +0530

    EJ Approved Merge PR from Develop branch to main branch (#1)
    * VueJS-CRUD-App Initial commit..
    
    WIP : [feat] [v0.1] : This app is still in beta-stage and improved versions and the initial working version will be done in future commit updates..
    Signed-off-by: Ebenezer Jesuraj <ebenezer@beezlabs.com>
    
    * VueJS-CRUD-App Update Commit
    
    WIP : [feat] [v0.2] : This app is still in beta-stage and improved versions and the initial working version will be done in future commit updates..
    Signed-off-by: Ebenezer Jesuraj <ebenezer@beezlabs.com>
    
    * WIP : [feat] [v0.3]
     This app is still in beta-stage and improved versions and the initial working version will be done in future commit updates..
    
    Note: VueJs-CRUD-App Update Commit
    Signed-off-by: Ebenezer Jesuraj <ebenezer@beezlabs.com>
    
    * WIP : [feat] [v0.4]
     This app is still in beta-stage and improved versions and the initial working version will be done in future commit updates..
    
    Note: VueJs-CRUD-App Update Commit
    Signed-off-by: Ebenezer Jesuraj <ebenezer@beezlabs.com>
    
    * WIP : [feat] [v0.5]
     This app is still in beta-stage and improved versions and the initial working version will be done in future commit updates..
    
    Note: VueJs-CRUD-App Update Commit
    Signed-off-by: Ebenezer Jesuraj <ebenezer@beezlabs.com>
    
    Signed-off-by: Ebenezer Jesuraj <ebenezer@beezlabs.com>

---
+++ src/components/CUD-Customer-Modal.vue
@@ -0,0 +1,44 @@
+<!-- Modal for adding new customers -->
+<!-- Modal for adding new customers -->
+<b-modal
+    ref="create-customer-modal"
+    hide-footer
+    size="xl"
+    title="New Customer"
+>
+<create-customer-form
+    @closeCreateModal="closeCreateModal"
+    @reloadDataTable="getCustomerData"
+    @showSuccessAlert="showAlertCreate"
+></create-customer-form>
+</b-modal>
+
+<!-- Modal for updating customers -->
+<b-modal
+    ref="edit-customer-modal"
+    hide-footer
+    size="xl"
+    title="Edit Customer"
+>
+<edit-customer-form
+    :customerId="customerId"
+    @closeEditModal="closeEditModal"
+    @reloadDataTable="getCustomerData"
+    @showSuccessAlert="showAlertUpdate"
+></edit-customer-form>
+</b-modal>
+
+<!-- Delete Customer Modal -->
+<b-modal
+    ref="delete-customer-modal"
+    hide-footer
+    size="md"
+    title="Confirm Deletion"
+>
+<delete-customer-modal
+    :customerId="customerId"
+    @closeDeleteModal="closeDeleteModal"
+    @reloadDataTable="getCustomerData"
+    @showDeleteAlert="showDeleteSuccessModal"
+></delete-customer-modal>
+</b-modal>
